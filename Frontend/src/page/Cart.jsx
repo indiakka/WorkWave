6d333cbac3c76254197c90788bb0db6a24354409
indiakka indiakka <78439130+indiakka@user.noreply.git>
2024-02-22 16:48:07 +0100

    añadir cambios del otro repositorio

--- Frontend/src/page/Cart.jsx
+++ Frontend/src/page/Cart.jsx
@@ -2,69 +2,45 @@ import React, { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
 import { useNavigate } from 'react-router-dom';
 
-const Cart = () => {
-  const { cartItems, getCartTotal, clearCart, updateItemQuantity, removeItem } = useContext(CartContext);
+const Cart = () =>
+{
+  const { cartItems, clearCart } = useContext( CartContext );
   const navigate = useNavigate();
 
-  const handleClearCart = () => {
+  const handleClearCart = () =>
+  {
     clearCart();
   };
 
-  const handleCheckout = () => {
-    alert('¡Gracias por tu compra!');
-    clearCart();
-    // Redirige a la página de inicio después de la alerta
-    navigate('/');
-  };
-
-  const handleUpdateQuantity = (itemId, amount) => {
-    updateItemQuantity(itemId, amount);
-  };
-
-  const handleRemoveItem = (itemId) => {
-    removeItem(itemId);
-  };
 
   return (
     <div className="cart-section">
-      <h1 className="section-title-cart">Resumen de Compra</h1>
+      <h1 className="section-title-cart">Tus inscripciones</h1>
       {cartItems.length === 0 ? (
-        <p>No hay productos en el carrito.</p>
+        <p>No te haz inscrito a ninguna oferta.</p>
       ) : (
         <>
           <ul className="cart-list">
-            {cartItems.map((item) => (
+            {cartItems.map( ( item ) => (
               <li key={item.id} className="cart-item">
                 <div>
-                  <p className="cart-item-name">{item.name}</p>
-                  <p className="cart-item-quantity">Cantidad: {item.quantity}</p>
-                  <p className="cart-item-price">Precio unitario: {item.price}€</p>
-                  <p className="cart-item-total">Total: {item.price * item.quantity}€</p>
+                  <p className="cart-item-name">{item.companyName}</p>
+                  <p className="cart-item-price">{item.jobName}</p>
+                  <p className="cart-item-total">Salario: {item.salary}$</p>
                 </div>
-                <div className="cart-item-actions">
-                  <button className="boton-small-pos" onClick={() => handleUpdateQuantity(item.id, 1)}>
-                    +
-                  </button>
-                  <button className="boton-small-neg" onClick={() => handleRemoveItem(item.id)}>
-                    -
-                  </button>
-                </div>
+                <div className="cart-item-actions"></div>
               </li>
-            ))}
+            ) )}
           </ul>
           <div className="cart-summary">
-            <p>Total del Carrito: {getCartTotal()}€</p>
-            </div>
+          </div>
           <div className="cart-actions">
-            <button className="Vaciar" onClick={handleClearCart}>Vaciar Carrito</button>
-            <button className="Pay" onClick={handleCheckout}>
-              Pagar
-            </button>
+            <button className="Vaciar" onClick={handleClearCart}>Eliminar todas las ofertas</button>
           </div>
         </>
       )}
       <div className="cart-actions">
-        <button className="Inicio" onClick={() => navigate('/')}>Volver a Inicio</button>
+        <button className="Inicio" onClick={() => navigate( '/' )}>Volver a Inicio</button>
       </div>
     </div>
   );
--- Frontend/src/page/Products.jsx
+++ Frontend/src/page/Products.jsx
@@ -7,50 +7,57 @@ import Card from '../Components/Card/Card';
 import { useNavigate } from 'react-router-dom';
 import CompShowAnnouncements from '../Components/CompShowOferts';
 
-const Pagination = () => {
+const Pagination = () =>
+{
   const navigate = useNavigate();
-  const [currentPage, setCurrentPage] = useState(1);
-  const [showModal, setShowModal] = useState(false);
-  const [selectedProduct, setSelectedProduct] = useState(null);
-  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [ currentPage, setCurrentPage ] = useState( 1 );
+  const [ showModal, setShowModal ] = useState( false );
+  const [ selectedProduct, setSelectedProduct ] = useState( null );
+  const [ selectedCategory, setSelectedCategory ] = useState( null );
 
-  const { addToCart, updateCartItemCount } = useContext(CartContext);
+  const { addToCart, updateCartItemCount } = useContext( CartContext );
 
   const productsPerPage = 4;
-  const startIndex = (currentPage - 1) * productsPerPage;
+  const startIndex = ( currentPage - 1 ) * productsPerPage;
   const endIndex = startIndex + productsPerPage;
 
   const filteredProducts = selectedCategory
-    ? productsData.filter((product) => product.category === selectedCategory)
+    ? productsData.filter( ( product ) => product.category === selectedCategory )
     : productsData;
 
-  const currentProducts = filteredProducts.slice(startIndex, endIndex);
-  const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
+  const currentProducts = filteredProducts.slice( startIndex, endIndex );
+  const totalPages = Math.ceil( filteredProducts.length / productsPerPage );
 
-  const handlePageChange = (newPage) => {
-    setCurrentPage(newPage);
+  const handlePageChange = ( newPage ) =>
+  {
+    setCurrentPage( newPage );
   };
 
-  const handleProductClick = (product) => {
-    setSelectedProduct(product);
-    setShowModal(true);
+  const handleProductClick = ( product ) =>
+  {
+    setSelectedProduct( product );
+    setShowModal( true );
   };
 
-  const closeModal = () => {
-    setShowModal(false);
+  const closeModal = () =>
+  {
+    setShowModal( false );
   };
 
-  const handleAddToCart = (product) => {
-    addToCart({ ...product, quantity: (product.quantity || 0) + 1 });
-    if (updateCartItemCount) {
+  const handleAddToCart = ( product ) =>
+  {
+    addToCart( { ...product, quantity: ( product.quantity || 0 ) + 1 } );
+    if ( updateCartItemCount )
+    {
       updateCartItemCount();
     }
-    alert(`¡${product.name} añadido al carrito!`);
+    alert( `¡${product.name} añadido al carrito!` );
   };
 
-  const handleCategoryChange = (category) => {
-    setCurrentPage(1);
-    setSelectedCategory(category);
+  const handleCategoryChange = ( category ) =>
+  {
+    setCurrentPage( 1 );
+    setSelectedCategory( category );
   };
 
 
@@ -60,32 +67,32 @@ const Pagination = () => {
       <h1 className="section-title"> Puedes escoger a tu próxima víctima...</h1>
       <section className='products'>
         <CategoryFilter
-          categories={getUniqueCategories(productsData)}
+          categories={getUniqueCategories( productsData )}
           onCategoryChange={handleCategoryChange}
         />
         <section className='slider'>
           <ul className="product-list">
-            {currentProducts.map((product) => (
+            {currentProducts.map( ( product ) => (
               <li key={product.id} className="product-item">
                 <h3 className="product-name">{product.name}</h3>
                 <img
                   src={product.imgUrl}
                   alt={product.name}
                   className="product-image"
-                  onClick={() => handleProductClick(product)}
+                  onClick={() => handleProductClick( product )}
                 />
                 <p className="product-price">{product.price}€</p>
-                <button className="add-to-cart-button" onClick={() => handleAddToCart(product)}>
+                <button className="add-to-cart-button" onClick={() => handleAddToCart( product )}>
                   Agregar al carrito
                 </button>
                 <Card productData={product} />
               </li>
-            ))}
+            ) )}
           </ul>
 
           <div className="pagination">
             <button
-              onClick={() => handlePageChange(currentPage - 1)}
+              onClick={() => handlePageChange( currentPage - 1 )}
               disabled={currentPage === 1}
               className="pagination-button"
             >
@@ -95,7 +102,7 @@ const Pagination = () => {
             <span className="pagination-info">{currentPage} de {totalPages}</span>
 
             <button
-              onClick={() => handlePageChange(currentPage + 1)}
+              onClick={() => handlePageChange( currentPage + 1 )}
               disabled={currentPage === totalPages}
               className="pagination-button"
             >
@@ -110,24 +117,24 @@ const Pagination = () => {
           )}
         </section>
 
-</section >
-<section className='discount'>
-  <div className='discount-text'>
-    <h2>
-        ¿Tienes una planta en apuros, triste y desanimada?
-        </h2>
-      <p> 
-        ¡No te preocupes, amigo! En Matoplantas.com te ofrecemos un descuento especial si nos traes tu planta que parece más un zombie que una belleza verde.
-       
-           No importa si está marchita, despeinada o con una crisis de identidad, aquí la recuperaremos...
-      </p>
-      <h3>
-        ¡A qué esperas para obtener tu descuento!
-      </h3>
-</div>
-<div><button class="btn" onClick={() => navigate('/Login')}>50% de Descuento</button></div>
+      </section >
+      <section className='discount'>
+        <div className='discount-text'>
+          <h2>
+            ¿Tienes una planta en apuros, triste y desanimada?
+          </h2>
+          <p>
+            ¡No te preocupes, amigo! En Matoplantas.com te ofrecemos un descuento especial si nos traes tu planta que parece más un zombie que una belleza verde.
 
-</section>
+            No importa si está marchita, despeinada o con una crisis de identidad, aquí la recuperaremos...
+          </p>
+          <h3>
+            ¡A qué esperas para obtener tu descuento!
+          </h3>
+        </div>
+        <div><button class="btn" onClick={() => navigate( '/Login' )}>50% de Descuento</button></div>
+
+      </section>
     </section>
   );
 };
@@ -135,6 +142,7 @@ const Pagination = () => {
 export default Pagination;
 
 // Función para obtener categorías únicas de los productos
-function getUniqueCategories(products) {
-  return Array.from(new Set(products.map((product) => product.category)));
+function getUniqueCategories ( products )
+{
+  return Array.from( new Set( products.map( ( product ) => product.category ) ) );
 }
